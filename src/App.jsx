c45ac741c6dab82eb97ebf5031bf48f8a72081a9
Asikur Rahman <mdasikur5893@gmail.com>
2025-09-30 00:53:13 +0600

    from set

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+const handle = (e)=>{
+  e.preventDefault()
+  console.log(e.target.name.value)
+  console.log(e.target.email.value)
+
+  }
+const App = () => {
+  return (
+    <div className='font-bold text-center text-2xl'>
+      <h1>React From</h1>
+      <form onSubmit={handle} >
+        <input className='border-2' type="text" name='name' placeholder='any text:-'/> <br />
+        <input className='border-2' type="email" name="email" id="" />
+        <input className='border-2 my-5' type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default App;
