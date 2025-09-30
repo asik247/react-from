56271a8ef3440a25098ff92d93ed67e2ee528011
Asikur Rahman <mdasikur5893@gmail.com>
2025-09-30 10:07:05 +0600

    control and condition

--- src/components/ControlField/ControlField.jsx
+++ src/components/ControlField/ControlField.jsx
@@ -5,10 +5,22 @@ const ControlField = () => {
 
     const onSubmitHandle = (e)=>{
         e.preventDefault()
+        if(password.length<6){
+            setError("plaes requer")
+        } else{
+            setError("")
+        }
     }
+    const [error,setError] = useState('')
     const [password,setPassword] = useState("");
     const ControlHandle = (e)=>{
-        console.log(e.target.value)
+        console.log(e.target.value);
+        setPassword(e.target.value);
+        // if(password.length <6){
+        //     setError("password must be 6 charecter")
+        // }else{
+        //     setError("")
+        // }
     }
 
 
@@ -16,12 +28,15 @@ const ControlField = () => {
     return (
         <div>
          <form onSubmit={onSubmitHandle}>
-            <input className='border-2 p-2 rounded-2xl' type="email" name="email" id="" placeholder='email' /> <br /><br />
+            <input className='border-2 p-2 rounded-2xl' type="email" name="email"  placeholder='email' /> <br /><br />
 
-            <input className='border-2 p-2 rounded-2xl' type="password" name="password" id="" onChange={ControlHandle} defaultValue={password} placeholder='password' /> <br /><br />
+            <input className='border-2 p-2 rounded-2xl' type="password" name="password" onChange={ControlHandle} defaultValue={password} placeholder='password' /> <br /><br />
 
             <input className='border-2 rounded-2xl p-2' type="submit" value="Submit" />
          </form>
+          <p style={{color:'red'}}>
+            <small>{error}</small>
+          </p>
         </div>
     );
 };
